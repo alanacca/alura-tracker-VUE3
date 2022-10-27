622aafe34affa7bd1e61ce049497cea6e7f9b073
alana <alana.araujo@tce.ma.gov.br>
2022-10-27 10:58:28 -0300

    teste

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioTarefa @aoSalvarTarefa="salvarTarefa"/>
+      <div class="lista">
+        <Tarefa v-for="(tarefa,index) in tarefas" :key="index" :tarefa="tarefa"/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue'
+import FormularioTarefa from './components/FormularioTarefa.vue'
+import Tarefa from './components/Tarefa.vue'
+import ITarefa from './interfaces/ITarefa'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    FormularioTarefa,
+    Tarefa,
+  },
+  data(){
+    return{
+      tarefas: [] as ITarefa[]
+    }
+  },
+  methods:{
+    salvarTarefa (tarefa: ITarefa){
+      this.tarefas.push(tarefa)
+    }
+  }
+});
+</script>
+
+<style>
+.lista{
+  padding: 1.25rem;
+}
+</style>
